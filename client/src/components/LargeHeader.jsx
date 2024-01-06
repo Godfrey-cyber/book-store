da21ae20253413072696665ac093007effe1e6ab
Godfrey Ndiritu <godfreyndiritu26@gmail.com>
2024-01-06 11:57:44 +0300

    minor header styling

--- client/src/components/LargeHeader.jsx
+++ client/src/components/LargeHeader.jsx
@@ -29,27 +29,26 @@ const LargeHeader = () => {
 
 	}, [scrolled])
 	return (
-		<nav className={`navbar ${scrolled ? 'scrolled' : ''} transition delay-300 flex h-[60px] relative justify-between items-center w-full bg-gray-100 shadow-xl shadow-gray-600 border-b z-10 border-gray-300 px-20`}>
+		<nav className={`navbar ${scrolled ? 'scrolled' : ''} large_header`}>
         	<div className="flex space-x-3 items-center">
 	        	<span onClick={() => navigate("/")} className="flex h-8 cursor-pointer w-auto">
 	        		<img className="w-full h-full" src="https://preview.colorlib.com/theme/abcbook/assets/img/logo/logo.png" />
 	        	</span>
         	</div>
 	        {/*SEARCH*/}
-	        <div className="flex justify-between items-center rounded-3xl border border-gray-500 w-96 h-11 p-1">
-	        	<input type="text" className="w-full h-full outline-none border-none rounded-3xl bg-transparent px-3 text-sm font-light text-gray-600" placeholder="Search book by author, title or publisher" />
-	        	<FaSearch className="hover:text-red-600 text-red-500 transition delay-400 text-2xl font-light cursor-pointer mr-3" />
+	        <div className="search_div">
+	        	<input type="text" className="search_input" placeholder="Search book by author, title or publisher" />
+	        	<FaSearch className="search_icon" />
 	        </div>
 	        {/*CTA'S*/}
 	        <div className="flex space-x-4 items-center">
-	        	
-	        	<p className="text-sm font-thin text-black cursor-pointer">FAQ</p>
-	        	<p className="text-sm font-thin text-black cursor-pointer">Track Order</p>
+	        	<p className="cta_p">FAQ</p>
+	        	<p className="cta_p">Track Order</p>
 	        	<span onClick={() => navigate("/cart_page")} className="cursor-pointer">
-	        		<BsCart className="hover:text-red-600 text-red-500 transition delay-400 text-2xl font-light cursor-pointer mr-3" />
-	        		<span className="flex items-center absolute bottom-7 right-48 bg-red-400 h-6 w-6 justify-center rounded-full text-center text-white text-xs border-2 border-white">{ items > 9 ? "9+" : items }</span>
+	        		<BsCart className="cta_icon" />
+	        		<span className="cta_span">{ items > 9 ? "9+" : items }</span>
 	        	</span>
-	        	<button onClick={() => navigate("/sign_in")} type="button" className="text-sm font-medium border border-red-400 hover:divide-red-500 hover:bg-white text-white rounded-3xl bg-red-400 rounded-3xl hover:text-red-500 px-5 py-2 transition-all delay-300 ease-in-out duration-300 cursor-pointer">Sign In</button>
+	        	<button onClick={() => navigate("/sign_in")} type="button" className="signin_btn">Sign In</button>
 	        </div>
         </nav>
 	)
--- client/src/components/SmallHeader.jsx
+++ client/src/components/SmallHeader.jsx
@@ -9,14 +9,14 @@ const SmalHeader = () => {
 				{/*//media & time*/}
 				<div className="flex justify-between items-center space-x-3">
 					<span className="flex space-x-2 text-gray-500 text-sm items-center">
-						<IoMdTime className="hover:text-red-600 text-gray-500 transition delay-400 text-sm cursor-pointer" />
+						<IoMdTime className="media_icon hover:text-red-600" />
 						<p className="text-sm font-light text-gray-600">Openning Hours 10.00 am - 6.00 pm</p>	
 					</span>
 					<span className="flex space-x-3 text-gray-600">|</span>
 					<div className="flex space-x-3 items-center" >
-						<FaFacebook className="hover:text-blue-600 text-gray-500 transition delay-400 text-sm cursor-pointer" />
-						<FaInstagram className="hover:text-yellow-600 text-gray-500 transition delay-400 text-sm cursor-pointer" />
-						<FaYoutube className="hover:text-red-600 text-gray-500 transition delay-400 text-sm cursor-pointer" />
+						<FaFacebook className="hover:text-blue-600 media_icon" />
+						<FaInstagram className="hover:text-yellow-600 media_icon" />
+						<FaYoutube className="hover:text-red-600 media_icon" />
 					</div>
 				</div>
 				{/*SELL A BOOK*/}
@@ -24,11 +24,11 @@ const SmalHeader = () => {
 				{/*//cta*/}
 				<div className="flex space-x-3 px-20 items-center">
 					<span className="flex space-x-2 font-light text-sm text-gray-600 items-center">
-						<FaPhoneAlt className="hover:text-red-600 text-gray-500 transition delay-400 text-sm cursor-pointer" />
+						<FaPhoneAlt className="hover:text-red-600 media_icon" />
 						<p className="text-sm font-light text-gray-600">reallygreat@gmail.com</p>	 
 					</span>
 					<span className="flex space-x-2 font-light text-sm text-gray-600 items-center">
-						<IoMdMail className="hover:text-red-600 text-gray-500 transition delay-400 text-sm cursor-pointer" />
+						<IoMdMail className="hover:text-red-600 media_icon" />
 						<p className="text-sm font-light text-gray-600">+254 7123 8456</p>
 					</span>
 				</div>
